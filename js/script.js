60f0204bd58e2efa46d62412fb0e8a422640acf9
Polad <polad.ibrahim27@gmail.com>
2017-03-12 20:49:55 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function countDown() {
+    var now = new Date();
+    var eventDate = new Date(2017, 2, 20);
+
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("day").innerHTML = d;
+    document.getElementById("hour").innerHTML = h;
+    document.getElementById("minute").innerHTML = m;
+    document.getElementById("second").innerHTML = s;
+
+    setTimeout(countDown, 1000);
+}
+
+countDown();
